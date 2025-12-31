a80c39840c489b0da15e574f0e636970cc8751e6
Francesco Derme <francesco.derme02@gmail.com>
2025-12-31 17:47:19 +0100

    Falling particle

--- include/particle.hpp
+++ include/particle.hpp
@@ -1,11 +1,26 @@
 #ifndef PARTICLE_HPP
 #define PARTICLE_HPP
 
-#include <external/SFML/Graphics.cpp>
+#include <SFML/Graphics.hpp>
+
+#include "SFML/System/Vector2.hpp"
 
 class Particle {
    public:
-    sf::Vector2f position;
-}
+    sf::Vector2f position, previous_position, acceleration;
 
-#endif PARTICLE_HPP  // PARTICLE_HPP
+    Particle(float x, float y)
+        : position{x, y}, previous_position(x, y), acceleration(0, 0) {}
+
+    void apply_force(const sf::Vector2f& force) { acceleration += force; }
+
+    void update(float time_step) {
+        // Verlet intehration: r_(n+1) = 2r_n - r_(n-1) + a_n * dt^2
+        sf::Vector2f velocity = position - previous_position;
+        previous_position = position;
+        position += velocity + acceleration * time_step * time_step;
+        acceleration = sf::Vector2f(0, 0);
+    }
+};
+
+#endif  // PARTICLE_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,54 @@
-// Verlet intehration: r_(n+1) = 2r_n - r_(n-1) + a_n * dt^2
+#include <SFML/Graphics.hpp>
+#include <vector>
 
-#include <iostream>
+#include "SFML/Graphics/CircleShape.hpp"
+#include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/System/Vector2.hpp"
+#include "SFML/Window/Keyboard.hpp"
+#include "particle.hpp"
+
+constexpr int WIDTH = 1080;
+constexpr int HEIGHT = 720;
+constexpr float PARTICLE_RADIUS = 30.0f;
+constexpr float GRAVITY = 9.8f;
+constexpr float TIME_STEP = 0.01f;
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
+    sf::Vector2u window_dimensions{WIDTH, HEIGHT};
+    sf::RenderWindow window(sf::VideoMode(window_dimensions),
+                            "Cloth simulation");
+
+    std::vector<Particle> particles;
+    particles.emplace_back(WIDTH / 2, HEIGHT / 2);
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+            else if (const auto* keyPressed =
+                         event->getIf<sf::Event::KeyPressed>()) {
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+                    window.close();
+            }
+        }
+
+        sf::Vector2f gravity_force(0, GRAVITY);
+        for (auto& particle : particles) {
+            particle.apply_force(gravity_force);
+            particle.update(TIME_STEP);
+        }
+
+        window.clear(sf::Color::Black);
+
+        for (const auto& particle : particles) {
+            sf::CircleShape circle(PARTICLE_RADIUS);
+            circle.setFillColor(sf::Color::White);
+            circle.setPosition(particle.position);
+            window.draw(circle);
+        }
+
+        window.display();
+    }
+
     return 0;
 }
